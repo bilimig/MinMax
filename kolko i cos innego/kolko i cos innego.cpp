5161963aba005c33c2a8eb9c62bd0b66e77d3f7c
Owczarek Kamil <bilimig@wp.pl>
2023-06-27 21:09:15 +0200

    Rewrited and finished

--- kolko i cos innego/kolko i cos innego.cpp
+++ kolko i cos innego/kolko i cos innego.cpp
@@ -1,317 +1,407 @@
 ﻿#include <iostream>
+#include <algorithm>
 #include <vector>
+#include <utility>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
-class TicTacToe {
-	int rozmiar;
-	int to_win;
-	string** board;
-	string currentplayer = "kolko";
-	bool is_game_ended = false;
-	bool is_game_drowed = false;
-	string winner = "Brak";
-	int currentpossition = 0;
-	void create_board() {
-		is_game_ended = false;
-		this->board = new string * [this->rozmiar];
-		for (size_t x = 0; x < this->rozmiar; x++)
-		{
-			board[x] = new string[this->rozmiar];
-			for (size_t y = 0; y < this->rozmiar; y++)
-			{
-				board[x][y] = "Puste";
-			}
-		}
-	}
+#define WIN 1000
+#define DRAW 0
+#define LOSS -1000
 
-public:
-	int ilepustych = rozmiar * rozmiar;
-	TicTacToe() {
+#define AI_MARKER 'O'
+#define PLAYER_MARKER 'X'
+#define EMPTY_SPACE '-'
 
-	}
+#define START_DEPTH 0
 
-	TicTacToe(int rozmiar, int to_win) {
+// Print game state
+void print_game_state(int state)
+{
+    if (WIN == state) { cout << "WIN" << endl; }
+    else if (DRAW == state) { cout << "DRAW" << endl; }
+    else if (LOSS == state) { cout << "LOSS" << endl; }
+}
 
-		this->rozmiar = rozmiar;
-		this->to_win = to_win;
-		create_board();
-		makemove();
-	}
+// All possible winning states
+std::vector<std::vector<std::pair<int, int>>> winning_states;
 
-	TicTacToe(TicTacToe& gra)
-	{
-		this->rozmiar = gra.rozmiar;
-		this->to_win = gra.to_win;
-		this->currentplayer = gra.currentplayer;
-		this->is_game_ended = gra.is_game_ended;
-		this->is_game_drowed = gra.is_game_drowed;
-		this->winner = gra.winner;
-		this->currentpossition = gra.currentpossition;
-		
-		this->board = new string * [this->rozmiar];
+// Print the current board state
+void print_board(std::vector<std::vector<char>>& board)
+{
+    cout << endl;
+    for (int i = 0; i < board.size(); ++i)
+    {
+        for (int j = 0; j < board[i].size(); ++j)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
-		for (size_t x = 0; x < this->rozmiar; x++)
-		{
-			this->board[x] = new string[this->rozmiar];
-			for (size_t y = 0; y < this->rozmiar; y++)
-			{
-				this->board[x][y] = gra.board[x][y];
-			}
-		}
+// Get all available legal moves (spaces that are not occupied)
+std::vector<std::pair<int, int>> get_legal_moves(std::vector<std::vector<char>>& board)
+{
+    std::vector<std::pair<int, int>> legal_moves;
+    for (int i = 0; i < board.size(); ++i)
+    {
+        for (int j = 0; j < board[i].size(); ++j)
+        {
+            if (board[i][j] != AI_MARKER && board[i][j] != PLAYER_MARKER)
+            {
+                legal_moves.push_back(std::make_pair(i, j));
+            }
+        }
+    }
 
-	}
+    return legal_moves;
+}
 
-	void wyswietl_tablice()
-	{
-		// zrobic ladniejsza 
-		for (size_t x = 0; x < rozmiar; x++)
-		{
-			for (size_t y = 0; y < rozmiar; y++)
-			{
-				cout << board[x][y] << " # ";
+// Check if a position is occupied
+bool position_occupied(std::vector<std::vector<char>>& board, std::pair<int, int> pos)
+{
+    std::vector<std::pair<int, int>> legal_moves = get_legal_moves(board);
 
-			}
-			cout << "\n";
+    for (int i = 0; i < legal_moves.size(); ++i)
+    {
+        if (pos.first == legal_moves[i].first && pos.second == legal_moves[i].second)
+        {
+            return false;
+        }
+    }
 
-		}
-	}
+    return true;
+}
 
-	void makemove()
-	{
-		bool ruch_udany = false;
-		wyswietl_tablice();
-		while (!ruch_udany)
-		{
-			int x;
-			int y;
-			cout << "Podaj wspolrzedne pola ktore chcesz wybrac\n";
-			cin >> y;
-			cin >> x;
+// Get all board positions occupied by the given marker
+std::vector<std::pair<int, int>> get_occupied_positions(std::vector<std::vector<char>>& board, char marker)
+{
+    std::vector<std::pair<int, int>> occupied_positions;
 
-			ruch_udany = move(x, y, currentplayer);
-		}
-		ilepustych--;
+    for (int i = 0; i < board.size(); ++i)
+    {
+        for (int j = 0; j < board[i].size(); ++j)
+        {
+            if (marker == board[i][j])
+            {
+                occupied_positions.push_back(std::make_pair(i, j));
+            }
+        }
+    }
 
-		if (!is_game_ended && !is_game_drowed)
-		{
-			if (currentplayer == "kolko")
-			{
-				currentplayer = "krzyz";
-			}
-			else if (currentplayer == "krzyz")
-			{
-				currentplayer = "kolko";
-			}
+    return occupied_positions;
+}
 
-			makemove();
-		}
+// Check if the board is full
+bool board_is_full(std::vector<std::vector<char>>& board)
+{
+    std::vector<std::pair<int, int>> legal_moves = get_legal_moves(board);
 
-		else if (is_game_ended)
-		{
-			wyswietl_tablice();
-			cout << "Wygrywa " << currentplayer << "\n";
-			
-			if (currentplayer == "kolko")
-			{
-				currentpossition = 1;
-			}
-			else if (currentplayer == "krzyz")
-			{
-				currentpossition = -1;
-			}
-			
-		}
+    return legal_moves.empty();
+}
 
-		else if (is_game_drowed)
-		{
-			wyswietl_tablice();
-			cout << "REMIS \n";
-		}
+// Check if a player has won the game
+bool is_winning_move(std::vector<std::vector<char>>& board, char marker, int length_to_win)
+{
+    int rows = board.size();
+    int cols = board[0].size();
 
-	}
+    // Check rows
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j <= cols - length_to_win; ++j)
+        {
+            bool win = true;
+            for (int k = 0; k < length_to_win; ++k)
+            {
+                if (board[i][j + k] != marker)
+                {
+                    win = false;
+                    break;
+                }
+            }
+            if (win)
+            {
+                return true;
+            }
+        }
+    }
 
-	bool move(int x, int y, string gracz)
-	{
-		if (board[x][y] != "Puste")
-		{
-			return false;
-		}
-		board[x][y] = gracz;
-		check_game_state();
-		return true;
-	}
+    // Check columns
+    for (int i = 0; i <= rows - length_to_win; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            bool win = true;
+            for (int k = 0; k < length_to_win; ++k)
+            {
+                if (board[i + k][j] != marker)
+                {
+                    win = false;
+                    break;
+                }
+            }
+            if (win)
+            {
+                return true;
+            }
+        }
+    }
 
-	void check_game_state()
-	{
-		bool is_drawed = true;
-		for (size_t x = 0; x < rozmiar; x++)
-		{
-			for (size_t y = 0; y < rozmiar; y++)
-			{
-				//jak beda bledy zobaczyc czy x z y sie nie jebnal albo rozmiar - x > to_win?
+    // Check diagonals (top-left to bottom-right)
+    for (int i = 0; i <= rows - length_to_win; ++i)
+    {
+        for (int j = 0; j <= cols - length_to_win; ++j)
+        {
+            bool win = true;
+            for (int k = 0; k < length_to_win; ++k)
+            {
+                if (board[i + k][j + k] != marker)
+                {
+                    win = false;
+                    break;
+                }
+            }
+            if (win)
+            {
+                return true;
+            }
+        }
+    }
 
-				if (check_column(x, y) || check_row(x, y) || check_diagonaldown(x, y) || check_diagonalup(x, y)) {
-					this->is_game_ended = true;
-				}
-				if (board[x][y] == "Puste") {
-					is_drawed = false;
-				}
+    // Check diagonals (top-right to bottom-left)
+    for (int i = 0; i <= rows - length_to_win; ++i)
+    {
+        for (int j = length_to_win - 1; j < cols; ++j)
+        {
+            bool win = true;
+            for (int k = 0; k < length_to_win; ++k)
+            {
+                if (board[i + k][j - k] != marker)
+                {
+                    win = false;
+                    break;
+                }
+            }
+            if (win)
+            {
+                return true;
+            }
+        }
+    }
 
-			}
-		}
-		this->is_game_drowed = is_drawed;
-	}
+    return false;
+}
 
-	bool check_column(int x, int y)
-	{
-		if (board[x][y] == "Puste") {
-			return false;
-		}
-		if (rozmiar - x < to_win)
-		{
-			return false;
-		}
+// Check if the game is won by any player
+bool game_is_won(std::vector<std::pair<int, int>>& occupied_positions, int size, int length_to_win)
+{
+    for (int i = 0; i < occupied_positions.size(); ++i)
+    {
+        int row = occupied_positions[i].first;
+        int col = occupied_positions[i].second;
 
-		for (size_t i = 1; i < to_win; i++)
-		{
-			if (board[x][y] != board[x + i][y] || board[x + i][y] == "Puste")
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+        std::vector<std::vector<char>> board = std::vector<std::vector<char>>(size, std::vector<char>(size, EMPTY_SPACE));
+        board[row][col] = AI_MARKER;
 
-	bool check_row(int x, int y)
-	{
-		if (board[x][y] == "Puste") {
-			return false;
-		}
-		if (rozmiar - y < to_win)
-		{
-			return false;
-		}
+        if (is_winning_move(board, AI_MARKER, length_to_win))
+        {
+            return true;
+        }
+    }
 
-		for (size_t i = 1; i < to_win; i++)
-		{
-			if (board[x][y] != board[x][y + i] || board[x][y + i] == "Puste")
-			{
-				return false;
-			}
-		}
-		return true;
+    return false;
+}
 
-	}
+// Get the opponent marker
+char get_opponent_marker(char marker)
+{
+    return (marker == AI_MARKER) ? PLAYER_MARKER : AI_MARKER;
+}
 
-	bool check_diagonaldown(int x, int y)
-	{
-		if (board[x][y] == "Puste") {
-			return false;
-		}
-		if (rozmiar - x < to_win || rozmiar - y < to_win)
-		{
-			return false;
-		}
+// Minimax algorithm with alpha-beta pruning
+int minimax(std::vector<std::vector<char>>& board, char marker, int depth, int alpha, int beta, int size, int length_to_win)
+{
+    std::vector<std::pair<int, int>> legal_moves = get_legal_moves(board);
 
-		for (size_t i = 1; i < to_win; i++)
-		{
-			if (board[x][y] != board[x + i][y + i] || board[x + i][y + i] == "Puste")
-			{
-				return false;
-			}
-		}
-		return true;
+    if (is_winning_move(board, AI_MARKER, length_to_win))
+    {
+        return WIN - depth;
+    }
+    else if (is_winning_move(board, PLAYER_MARKER, length_to_win))
+    {
+        return LOSS + depth;
+    }
+    else if (legal_moves.empty())
+    {
+        return DRAW;
+    }
 
-	}
+    if (depth == START_DEPTH)
+    {
+        cout << "AI is thinking..." << endl;
+    }
 
-	bool check_diagonalup(int x, int y)
-	{
-		if (board[x][y] == "Puste") {
-			return false;
-		}
-		if (rozmiar - y < to_win || x < to_win - 1)
-		{
-			return false;
-		}
+    int best_score;
+    if (marker == AI_MARKER)
+    {
+        best_score = LOSS;
 
-		for (size_t i = 1; i < to_win; i++)
-		{
-			if (board[x][y] != board[x - i][y + i] || board[x - i][y + i] == "Puste")
-			{
-				return false;
-			}
-		}
-		return true;
+        for (int i = 0; i < legal_moves.size(); ++i)
+        {
+            int row = legal_moves[i].first;
+            int col = legal_moves[i].second;
 
-	}
-	
-};
+            board[row][col] = marker;
 
-struct coordinats {
-	int x;
-	int y;
-	coordinats() {}
-	coordinats(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
-};
+            int score = minimax(board, get_opponent_marker(marker), depth + 1, alpha, beta, size, length_to_win);
+            best_score = std::max(best_score, score);
 
-class MinMax {
-	
-	int current_possition=0;
-	string current_player;
-	int alfa=0;
-	int beta=0;
-	int depth = 9;
-	TicTacToe *gra_copy;
-	coordinats current_coordinats;
-	coordinats _coordinats;
+            board[row][col] = EMPTY_SPACE;
 
-public:	
-	MinMax(TicTacToe& gra, int current_possition, string current_player)
-	{
-		gra_copy = new TicTacToe(gra);
-		this ->current_possition = current_possition;
-		this->current_player = current_player;
-		
-	}
+            alpha = std::max(alpha, best_score);
+            if (beta <= alpha)
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        best_score = WIN;
 
-	int min_max_algorithm() {
-		if (current_possition != 0) {
-			return current_possition;
-		}
+        for (int i = 0; i < legal_moves.size(); ++i)
+        {
+            int row = legal_moves[i].first;
+            int col = legal_moves[i].second;
 
-		if (current_player == "kolko")
-		{
-			current_possition = -INFINITY;
-			for (size_t i = 0; i < gra_copy->ilepustych; i++)
-			{
-				
+            board[row][col] = marker;
 
+            int score = minimax(board, get_opponent_marker(marker), depth + 1, alpha, beta, size, length_to_win);
+            best_score = std::min(best_score, score);
 
-			}
-		}
+            board[row][col] = EMPTY_SPACE;
 
-	}
-};
-	//alfa beta z cieciami (pozycja, gracz, alfa, beta)
-	// glebokosc?
-	// wykonanie kazdego mozliwego ruchu i dla niego rekurencja alfa beta....
-	// ilosc ruchow? odwrotnie?  
-	//
+            beta = std::min(beta, best_score);
+            if (beta <= alpha)
+            {
+                break;
+            }
+        }
+    }
 
+    return best_score;
+}
+
+// Get the optimal move for the AI
+std::pair<int, int> get_optimal_move(std::vector<std::vector<char>>& board, int size, int length_to_win)
+{
+    std::vector<std::pair<int, int>> legal_moves = get_legal_moves(board);
+
+    int best_score = LOSS;
+    std::pair<int, int> best_move;
+
+    for (int i = 0; i < legal_moves.size(); ++i)
+    {
+        int row = legal_moves[i].first;
+        int col = legal_moves[i].second;
+
+        board[row][col] = AI_MARKER;
+
+        int score = minimax(board, PLAYER_MARKER, START_DEPTH + 1, LOSS, WIN, size, length_to_win);
+        if (score > best_score)
+        {
+            best_score = score;
+            best_move = std::make_pair(row, col);
+        }
+
+        board[row][col] = EMPTY_SPACE;
+    }
+
+    return best_move;
+}
+
+// Play the game
+void play_game(int size, int length_to_win)
+{
+    std::vector<std::vector<char>> board(size, std::vector<char>(size, EMPTY_SPACE));
+
+    int current_turn = 1;  // 1 for AI, 2 for player
+
+    while (true)
+    {
+        if (current_turn == 1)  // AI's turn
+        {
+            std::pair<int, int> ai_move = get_optimal_move(board, size, length_to_win);
+            int row = ai_move.first;
+            int col = ai_move.second;
+
+            board[row][col] = AI_MARKER;
+
+            cout << "AI's move:" << endl;
+            print_board(board);
+
+            if (is_winning_move(board, AI_MARKER, length_to_win))
+            {
+                cout << "AI wins!" << endl;
+                break;
+            }
+
+            if (board_is_full(board))
+            {
+                cout << "It's a draw!" << endl;
+                break;
+            }
+
+            current_turn = 2;
+        }
+        else if (current_turn == 2)  // Player's turn
+        {
+            int row, col;
+            cout << "Enter your move (row and column): ";
+            cin >> row >> col;
+
+            while (row < 0 || row >= size || col < 0 || col >= size || board[row][col] != EMPTY_SPACE)
+            {
+                cout << "Invalid move. Enter your move (row and column): ";
+                cin >> row >> col;
+            }
+
+            board[row][col] = PLAYER_MARKER;
+
+            cout << "Your move:" << endl;
+            print_board(board);
+
+            if (is_winning_move(board, PLAYER_MARKER, length_to_win))
+            {
+                cout << "You win!" << endl;
+                break;
+            }
+
+            if (board_is_full(board))
+            {
+                cout << "It's a draw!" << endl;
+                break;
+            }
+
+            current_turn = 1;
+        }
+    }
+}
 
 int main()
 {
+    int size, length_to_win;
+    cout << "Enter the size of the board: ";
+    cin >> size;
+    cout << "Enter the length to win: ";
+    cin >> length_to_win;
 
+    play_game(size, length_to_win);
 
-	TicTacToe gra(4, 4);
-
-
-
-
-
+    return 0;
 }
-
